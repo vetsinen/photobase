b4c3164e60b727949f59bef4d3cd13dc880e3943
vetsinen <vetsinen@gmail.com>
2018-11-09 13:52:25 +0200

    json photobase added

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import os, time
-from stat import * # ST_SIZE etc
+from stat import *  # ST_SIZE etc
+import json
 
-file = ".gitignore"
+default = []
+bytes = 0
+for entry in os.scandir('photos/'):
+    info = entry.stat()
+    rec = {'name': entry.name, "modified": int(info.st_mtime), 'size': info.st_size, 'url':
+        'https://vetsinen.github.io/photobase/photos/' + entry.name}
+    bytes += info.st_size
+    default.append(rec)
 
-try:
-    st = os.stat(file)
-except IOError:
-    print ("failed to get information about", file)
-else:
-    print(st)
-    # print ("file size:", st[ST_SIZE])
-    # print ("file modified:", time.asctime(time.localtime(st[ST_MTIME])))
+# print(json.dumps(default))
+
+bysizea = sorted(default, key=lambda k: k['size'])
+meta = {'bytes': bytes,'default':default, 'bysize': sorted(default, key=lambda k: k['size'])
+    , 'byfilename': sorted(default, key=lambda k: k['name'])
+    , 'bynamelength': sorted(default, key=lambda k: len(k['name']))}
+with open('base.json', 'w') as f:
+    f.write(json.dumps(meta))
